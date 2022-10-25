e08870f751ca08299ca8048f39914578d651e211
Ryan <ryan@7-77.eu>
2022-10-25 03:12:21 +0200

    x

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,25 @@
 @php
    $date = now();
-   $cache_put = \Illuminate\Support\Facades\Cache::put('cache_test', $date, 10);
-   $cache_retrieve = \Illuminate\Support\Facades\Cache::get('cache_test');
+   try {
+	$cache_put = \Illuminate\Support\Facades\Cache::put('cache_test', $date, 10);
+        $cache_retrieve = \Illuminate\Support\Facades\Cache::get('cache_test');
+   } catch(Exception $e){
+	$cache_retrieve = NULL;
+        $cache_error = $e->getMessage();
+   }
+
    $cache_test = $cache_retrieve === NULL ? true : false;
+   $cache_test_verbose = $cache_test === true ? "works" : $cache_error;
 
    try{
         $db = DB::connection()->getDatabaseName();
+	$db_test = true;
    }catch(Exception $e){
    	$db = $e->getMessage();
+	$db_test = false;
    }
+   $db_test_verbose = $db_test === true ? "works" : $db;
 @endphp
 
-<p><strong>Cache Test:</strong> {{ $cache_test }}</p>
-<p><strong>Database Connection:</strong> {{ $db }}</p>
+<p><strong>Cache Test:</strong> {{ $cache_test_verbose }}</p>
+<p><strong>Database Connection:</strong> {{ $db_test_verbose }}</p>
