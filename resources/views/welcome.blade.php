4eb9e512825e94d0e280279a72f49048e686a694
Ryan <ryan@7-77.eu>
2022-10-25 03:07:34 +0200

    x

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,14 @@
    $date = now();
    $cache_put = \Illuminate\Support\Facades\Cache::put('cache_test', $date, 10);
    $cache_retrieve = \Illuminate\Support\Facades\Cache::get('cache_test');
-   $cache_test = $cache_retrieve === NULL ? 'passed' : $cache_retrieve;
+   $cache_test = $cache_retrieve === NULL ? true : false;
 
+   try{
+        $db = DB::connection()->getDatabaseName();
+   }catch(Exception $e){
+   	$db = $e->getMessage();
+   }
 @endphp
 
-Cache Test: {{ $cache_test }}
-
+<p><strong>Cache Test:</strong> {{ $cache_test }}</p>
+<p><strong>Database Connection:</strong> {{ $db }}</p>
