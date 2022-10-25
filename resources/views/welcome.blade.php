6dd27e7d853681eeed3bde1dbf612f0cb4eb26f3
Ryan <ryan@7-77.eu>
2022-10-25 02:55:09 +0200

    x

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @php
    $date = now();
-   $cache_put = \Illuminate\Support\Facades\Cache::put('cache_test', $data, 10);
+   $cache_put = \Illuminate\Support\Facades\Cache::put('cache_test', $date, 10);
    $cache_retrieve = \Illuminate\Support\Facades\Cache::get('cache_test');
    $cache_test = $cache_retrieve === NULL ? 'passed' : $cache_retrieve;
 
